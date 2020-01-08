0a6d5f67109d463325f5d63d82e776dfaf7c336f
Carmine12328 <carmine12328@gmail.com>
2020-01-08 20:56:41 +0100

    Complete Game. With graphics(start scene.core game. win scene)

--- Assets/Scripts/Numberer.cs
+++ Assets/Scripts/Numberer.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Numberer : MonoBehaviour
 {
+    public Text guessText;
+    int max = 1000;
+    int min = 1;
+    int guess;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        NextGuess();
+        WriteGuess();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void WriteGuess()
     {
-        
+        guessText.text = guess.ToString();
+    }
+
+    private void NextGuess()
+    {
+        guess = Random.Range(min, max);
+    }
+
+    public void onPressHigher()
+    {
+        min = guess;
+        NextGuess();
+        WriteGuess();
+
+    }
+
+    public void onPressLower()
+    {
+        max = guess;
+        NextGuess();
+        WriteGuess();
+    }
+
+    public void onPressCorrect()
+    {
+        SceneManager.LoadScene(2);
     }
 }
---
+++ Assets/Scripts/Start_Page_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Page_script : MonoBehaviour
+{
+    public void onPressStart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void onPressQuit()
+    {
+        Application.Quit();
+    }
+
+    public void onPressTryAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
